1a4d736274dc7de41b040ed59f3ef86b5878a0ad
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-12-09 13:20:55 -0500

    Solve day 5 JS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # advent of code 2018
 
-solving advent challenges in rust: [adventofcode.com/2018](https://adventofcode.com/2018)
+solving advent challenges in javascript: [adventofcode.com/2018](https://adventofcode.com/2018)
---
+++ day5/index.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+let input = fs.readFileSync(__dirname + '/day5.txt', 'utf8')
+  .split('')
+
+function willReact (a, b) {
+  if (a === '$' || b === '$') return false
+  if (a.toLowerCase() === b.toLowerCase()) {
+    if (Math.abs(a.charCodeAt(0) - b.charCodeAt(0)) === 32) {
+      // only different case reacts
+      return true
+    }
+  }
+  return false
+}
+
+function react (s) {
+  let len = s.length
+  for (let i = 0; i < s.length - 1; i++) {
+    let left = i
+    let right = i+1
+    while (left >= 0 && right < s.length) {
+      if (willReact(s[left], s[right])) {
+        s[left] = '$'
+        s[right] = '$'
+        left--
+        right++
+      } else {
+        break
+      }
+    }
+  }
+  let reacted = s.filter(x => x !== '$')
+  if (len !== reacted.length) {
+    return react(reacted)
+  }
+  return reacted.length
+}
+
+function improveReaction (input) {
+  // keep track of what we've tried to remove
+  let removed = new Map()
+  for (let c of input) {
+    let key = c.toLowerCase()
+    if (removed.has(key)) continue
+    let filtered = input.filter(x => x.toLowerCase() !== key)
+    let length = react(filtered)
+    removed.set(key, length)
+  }
+  // get smallest entry
+  let min = Infinity
+  for (let [_, length] of removed) {
+    if (length < min) min = length
+  }
+  return min
+}
+
+console.log('solution 1:', react(input))
+console.log('solution 2:', improveReaction(input))
