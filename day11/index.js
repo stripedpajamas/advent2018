40788509d43a33a5f6271551ff53d8e2d8bdc9d2
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-12-11 11:08:12 -0500

    Lint day 11

--- day11/index.js
+++ day11/index.js
@@ -24,12 +24,12 @@ function findPowerGroup (grid, size) {
   // looking for a sizexsize group of max power
   let max = -Infinity
   let cell
-  for (let row = 1; row <= 300-size; row++) {
-    for (let col = 1; col <= 300-size; col++) {
+  for (let row = 1; row <= 300 - size; row++) {
+    for (let col = 1; col <= 300 - size; col++) {
       let power = 0
       for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-          power += grid[row+i][col+j]
+          power += grid[row + i][col + j]
         }
       }
       if (power > max) {
@@ -51,7 +51,7 @@ function calculateGroup (grid, row, col, size, computed) {
     computed[row][col][size] = grid[row][col]
     return computed[row][col][size]
   }
-  let previous = calculateGroup(grid, row, col, size-1, computed)
+  let previous = calculateGroup(grid, row, col, size - 1, computed)
   // add the difference of the squares
   let total = previous
   for (let c = 0; c < size; c++) {
@@ -67,15 +67,15 @@ function calculateGroup (grid, row, col, size, computed) {
 }
 
 function findBestSizedGroup (grid) {
-  // a data structure of 
+  // a data structure of
   // row -> col -> size -> power
   let computed = {}
   let max = -Infinity
   let cell
   let size
   for (let s = 1; s <= 300; s++) {
-    for (let row = 1; row <= 300-s; row++) {
-      for (let col = 1; col <= 300-s; col++) {
+    for (let row = 1; row <= 300 - s; row++) {
+      for (let col = 1; col <= 300 - s; col++) {
         let p = calculateGroup(grid, row, col, s, computed)
         if (p > max) {
           max = p
@@ -93,4 +93,3 @@ let grid = getFuelGrid(serialNumber)
 
 console.log('\tsolution 1:', findPowerGroup(grid, 3))
 console.log('\tsolution 2:', findBestSizedGroup(grid))
-
